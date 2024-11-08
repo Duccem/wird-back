2034fad208933439cb09dedeb12f54855614dbe8
Jose Veliz <ducen29@gmail.com>
2024-11-08 17:27:46 -0400

    Prepare api to docker

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -3,7 +3,9 @@ import { createClient, RedisClientType } from "redis";
 let client: RedisClientType;
 
 const connect = async () => {
-  client = (await createClient()
+  client = (await createClient({
+    url: process.env.REDIS_URL || "redis://localhost:6379",
+  })
     .on("error", (err) => console.log("Redis Client Error", err))
     .on("connect", () => console.log("Redis Client Connected"))
     .connect()) as unknown as RedisClientType;
